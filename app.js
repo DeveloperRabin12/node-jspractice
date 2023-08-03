06ff7a8ff2d3b7b51791b6496e3211c3b14297d3
Rabin Gapi <74033795+DeveloperRabin12@users.noreply.github.com>
2023-08-03 21:00:26 +0545

    about get res req nodemon

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require ('express')
 const app = express()
 
 
+app.get('/',(req,res)=>{
+    res.send("<h2>hello nodejs i want</h2>");
+});
+
+app.get('/about',(req,res)=>{
+    res.send("this is about");
+});
+
+app.get('/contact',(req,res)=>{
+    res.send("this is from the contact page");
+});
+
+
+
 app.listen(4000, function(){
     console.log("Node js project has been stared")
 });
