ef5db0dedb603970e5665396cb12b39a1ae0bf49
Rabin Gapi <74033795+DeveloperRabin12@users.noreply.github.com>
2023-08-04 20:57:10 +0545

    ejs ,cls,ssr

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 const express = require ('express')
 const app = express()
 
+//setting up ejs 
+app.set('view engine','ejs' )
+
+
 
 app.get('/',(req,res)=>{
-    res.send("<h2>hello nodejs i want</h2>");
+    res.render('home',{name:"Rabin"})
+    res.render('home',{name:"Rabin",age:22})
+   
 });
 
 app.get('/about',(req,res)=>{
-    res.send("this is about");
+    res.render('about.ejs')
+   
 });
 
 app.get('/contact',(req,res)=>{
-    res.send("this is from the contact page");
+    res.send("<h2>hello nodejs i want</h2>");
+   
+   
 });
 
 
-
+//4000 is a port number as user defined
 app.listen(4000, function(){
     console.log("Node js project has been stared")
 });
